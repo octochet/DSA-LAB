742e1b7c2836aa73e7b34493588ab9b220a62b3c
octochet <cs21b017@iittp.ac.in>
2023-03-14 16:28:32 +0530

    add lab 07 and 08

---
+++ Lab_07_28feb/avl.cpp
@@ -0,0 +1,146 @@
+#include <algorithm>  /// for std::max
+#include <iostream>   /// for std::cout
+#include <queue>      /// for std::queue
+
+using namespace std;
+
+struct node {
+    int data;
+    int height;
+    struct node *left;
+    struct node *right;
+};
+
+node *createNode(int data) {
+    node *nn = new node();
+    nn->data = data;
+    nn->height = 0;
+    nn->left = nullptr;
+    nn->right = nullptr;
+    return nn;
+}
+
+int height(node *root) {
+    if (root == nullptr) {
+        return 0;
+    }
+    return 1 + std::max(height(root->left), height(root->right));
+}
+
+int getBalance(node *root) { return height(root->left) - height(root->right); }
+
+node *rightRotate(node *root) {
+    node *t = root->left;
+    node *u = t->right;
+    t->right = root;
+    root->left = u;
+    return t;
+}
+
+node *leftRotate(node *root) {
+    node *t = root->right;
+    node *u = t->left;
+    t->left = root;
+    root->right = u;
+    return t;
+}
+
+node *minValue(node *root) {
+    if (root->left == nullptr) {
+        return root;
+    }
+    return minValue(root->left);
+}
+
+node *insert(node *root, int item) {
+    if (root == nullptr) {
+        return createNode(item);
+    }
+    if (item < root->data) {
+        root->left = insert(root->left, item);
+    } else {
+        root->right = insert(root->right, item);
+    }
+    int b = getBalance(root);
+    if (b > 1) {
+        if (getBalance(root->left) < 0) {
+            root->left = leftRotate(root->left);  // Left-Right Case
+        }
+        return rightRotate(root);  // Left-Left Case
+    } else if (b < -1) {
+        if (getBalance(root->right) > 0) {
+            root->right = rightRotate(root->right);  // Right-Left Case
+        }
+        return leftRotate(root);  // Right-Right Case
+    }
+    return root;
+}
+
+node *deleteNode(node *root, int element) {
+    if (root == nullptr) {
+        return root;
+    }
+    if (element < root->data) {
+        root->left = deleteNode(root->left, element);
+    } else if (element > root->data) {
+        root->right = deleteNode(root->right, element);
+
+    } else {
+        // Node to be deleted is leaf node or have only one Child
+        if (!root->right || !root->left) {
+            node *temp = !root->right ? root->left : root->right;
+            delete root;
+            return temp;
+        }
+        // Node to be deleted have both left and right subtrees
+        node *temp = minValue(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+    // Balancing Tree after deletion
+    return root;
+}
+
+void deleteAllNodes(const node *const root) {
+    if (root) {
+        deleteAllNodes(root->left);
+        deleteAllNodes(root->right);
+        delete root;
+    }
+}
+
+void levelOrder(node *root) {
+    queue<node *> q;
+    q.push(root);
+    while (!q.empty()) {
+        root = q.front();
+        std::cout << root->data << " ";
+        q.pop();
+        if (root->left) {
+            q.push(root->left);
+        }
+        if (root->right) {
+            q.push(root->right);
+        }
+    }
+}
+
+
+
+int main() {
+    // Testing AVL Tree
+    node *root = nullptr;
+    int i = 0;
+    for (i = 1; i <= 7; i++) root = insert(root, i);
+    cout << "LevelOrder: ";
+    levelOrder(root);
+    root = deleteNode(root, 1);  // Deleting key with value 1
+    cout << "\nLevelOrder: ";
+    levelOrder(root);
+    root = deleteNode(root, 4);  // Deletin key with value 4
+    cout << "\nLevelOrder: ";
+    levelOrder(root);
+    deleteAllNodes(root);
+    cout<<"\n";
+    return 0;
+}
---
+++ Lab_07_28feb/test.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+// struct to represent vertical and horizontal lines
+struct VerticalLine {
+    int x, y1, y2;
+};
+
+struct HorizontalLine {
+    int y, x1, x2;
+};
+
+// struct to represent a node in the AVL tree
+struct Node {
+    HorizontalLine line;
+    int height;
+    Node* left;
+    Node* right;
+};
+
+// function to calculate the height of a node
+int height(Node* node) {
+    return node == nullptr ? -1 : node->height;
+}
+
+// function to calculate the balance factor of a node
+int balanceFactor(Node* node) {
+    return node == nullptr ? 0 : height(node->left) - height(node->right);
+}
+
+// function to rotate a subtree to the left
+Node* rotateLeft(Node* node) {
+    Node* newRoot = node->right;
+    node->right = newRoot->left;
+    newRoot->left = node;
+    node->height = max(height(node->left), height(node->right)) + 1;
+    newRoot->height = max(height(newRoot->left), height(newRoot->right)) + 1;
+    return newRoot;
+}
+
+// function to rotate a subtree to the right
+Node* rotateRight(Node* node) {
+    Node* newRoot = node->left;
+    node->left = newRoot->right;
+    newRoot->right = node;
+    node->height = max(height(node->left), height(node->right)) + 1;
+    newRoot->height = max(height(newRoot->left), height(newRoot->right)) + 1;
+    return newRoot;
+}
+
+// function to balance a node
+Node* balance(Node* node) {
+    int bf = balanceFactor(node);
+    if (bf > 1) {
+        if (balanceFactor(node->left) < 0) {
+            node->left = rotateLeft(node->left);
+        }
+        node = rotateRight(node);
+    } else if (bf < -1) {
+        if (balanceFactor(node->right) > 0) {
+            node->right = rotateRight(node->right);
+        }
+        node = rotateLeft(node);
+    }
+    return node;
+}
+
+// function to insert a horizontal line into the AVL tree
+Node* insert(Node* root, const HorizontalLine& line) {
+    if (root == nullptr) {
+        root = new Node{line, 0, nullptr, nullptr};
+    } else if (line.y < root->line.y) {
+        root->left = insert(root->left, line);
+    } else {
+        root->right = insert(root->right, line);
+    }
+    root->height = max(height(root->left), height(root->right)) + 1;
+    return balance(root);
+}
+
+// Define the AVL tree
+Node* activeLines = nullptr;
+
+// Define the line sweep algorithm
+set<pair<int, int>> findIntersections(const vector<VerticalLine>& verticalLines, const vector<HorizontalLine>& horizontalLines) {
+    // Process each vertical line in order
+    set<pair<int, int>> intersections;
+    for (const auto& vl : verticalLines) {
+        // Add any intersecting horizontal lines to the AVL tree
+        for (const auto& hl : horizontalLines) {
+            if (hl.y >= vl.y1 && hl.y <= vl.y2 && hl.x1 <= vl.x) {
+                activeLines = insert(activeLines, hl);
+            }
+        }
+
+        // Remove any non-intersecting horizontal lines from the AVL tree
+        for (auto it = activeLines; it != nullptr;) {
+            const auto& hl = it->line;
+            if (hl.y > vl.y2) {
+                break;
+            }
+            if (hl.y >= vl.y1 && hl.y <= vl.y2 && hl.x2 >= vl.x) {
+                intersections.insert({vl.x, hl.y});
+            }
+            it = it->right;
+        }
+
+        // Find any intersections between the active horizontal lines and the vertical line
+        for (auto it = activeLines; it != nullptr; it = it->right) {
+            const auto& hl = it->line;
+            if (hl.y > vl.y2) {
+                break;
+            }
+            if (hl.y >= vl.y1 && hl.y <= vl.y2 && hl.x2 >= vl.x) {
+                intersections.insert({vl.x, hl.y});
+            }
+        }
+    }
+    return intersections;
+}
+
+// Example usage:
+int main() {
+    vector<VerticalLine> verticalLines = {{1, 1, 5}, {3, 2, 6}, {5, 4, 8}, {-2, 1, 5}};  // x, y1, y2  
+    vector<HorizontalLine> horizontalLines = {{2, -3, 4}, {4, 1, 5}, {7, 3, 6}};  // y, x1, x2
+    sort(verticalLines.begin(), verticalLines.end(), [](const VerticalLine& a, const VerticalLine& b) { return a.x < b.x; } );
+    set<pair<int, int>> intersections = findIntersections(verticalLines, horizontalLines);
+    cout << "Intersections:" << endl;
+    for (const auto& p : intersections) {
+        cout << "(" << p.first << ", " << p.second << ")" << endl;
+    }
+    return 0;
+}
+           
