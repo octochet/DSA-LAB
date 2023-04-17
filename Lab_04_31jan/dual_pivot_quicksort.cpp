3429450338ced2349c523413684190f466f6ec0a
octochet <cs21b017@iittp.ac.in>
2023-04-17 12:35:57 +0530

    dual-pivot quicksort completed

--- Lab_04_31jan/dual_pivot_quicksort.cpp
+++ Lab_04_31jan/dual_pivot_quicksort.cpp
@@ -3,121 +3,59 @@
 
 using namespace std;
 
-//dual pivot stable quicksort
-
-void dual_pivot_quicksort(int *arr, int left, int right)
+//stable quicksort
+vector<int> stableDualPivotQuickSort(vector<int> arr, int low, int high)
 {
-    if (left < right)
+    int n = high - low + 1;
+    if(n <= 1)
     {
-        int pivot1 = arr[left];
-        int pivot2 = arr[right];
-        if (pivot1 > pivot2)
-        {
-            int temp = pivot1;
-            pivot1 = pivot2;
-            pivot2 = temp;
-        }
-        int i = left + 1;
-        int j = right - 1;
-        int k = i;
-        while (k <= j)
-        {
-            if (arr[k] < pivot1)
-            {
-                int temp = arr[i];
-                arr[i] = arr[k];
-                arr[k] = temp;
-                i++;
-                k++;
-            }
-            else if (arr[k] >= pivot1 && arr[k] <= pivot2)
-            {
-                k++;
-            }
-            else
-            {
-                int temp = arr[j];
-                arr[j] = arr[k];
-                arr[k] = temp;
-                j--;
-            }
-        }
-        i--;
-        j++;
-        arr[left] = arr[i];
-        arr[i] = pivot1;
-        arr[right] = arr[j];
-        arr[j] = pivot2;
-        dual_pivot_quicksort(arr, left, i - 1);
-        dual_pivot_quicksort(arr, i + 1, j - 1);
-        dual_pivot_quicksort(arr, j + 1, right);
+        return arr;
     }
+    vector<int> left;
+    vector<int> right;
+    vector<int> middle;
+
+    int pivot1 = arr[low];
+    int pivot2 = arr[high];
+
+    for(int i=1; i<n-1; i++)
+    {
+        if(arr[i] < pivot1)
+        {
+            left.push_back(arr[i]);
+        }
+        else if(arr[i] > pivot2)
+        {
+            right.push_back(arr[i]);
+        }
+        else
+        {
+            middle.push_back(arr[i]);
+        }
+    }
+
+    vector<int> sorted;
+    left = stableDualPivotQuickSort(left, 0, left.size()-1);
+    sorted.insert(sorted.end(), left.begin(), left.end());
+    sorted.push_back(pivot1);
+    middle = stableDualPivotQuickSort(middle, 0, middle.size()-1);
+    sorted.insert(sorted.end(), middle.begin(), middle.end());
+    sorted.push_back(pivot2);
+    right = stableDualPivotQuickSort(right, 0, right.size()-1);
+    sorted.insert(sorted.end(), right.begin(), right.end());
+
+    return sorted;
 }
 
-void vector_sort(vector<pair<int,int>>& v, int left, int right)
-{
-    if (left < right)
-    {
-        int pivot1 = v[left].first;
-        int pivot2 = v[right].first;
-        if (pivot1 > pivot2)
-        {
-            int temp = pivot1;
-            pivot1 = pivot2;
-            pivot2 = temp;
-        }
-        int i = left + 1;
-        int j = right - 1;
-        int k = i;
-        while (k <= j)
-        {
-            if (v[k].first < pivot1)
-            {
-                pair<int,int> temp = v[i];
-                v[i] = v[k];
-                v[k] = temp;
-                i++;
-                k++;
-            }
-            else if (v[k].first >= pivot1 && v[k].first <= pivot2)
-            {
-                k++;
-            }
-            else
-            {
-                pair<int,int> temp = v[j];
-                v[j] = v[k];
-                v[k] = temp;
-                j--;
-            }
-        }
-        i--;
-        j++;
-        v[left] = v[i];
-        v[i].first = pivot1;
-        v[right] = v[j];
-        v[j].first = pivot2;
-        vector_sort(v, left, i - 1);
-        vector_sort(v, i + 1, j - 1);
-        vector_sort(v, j + 1, right);
-    }
-}
 
 int main()
 {
-    int arr[] = { 5, 3, 8, 4, 2, 7, 1, 10, 5 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    dual_pivot_quicksort(arr, 0, n - 1);
+    vector<int> arr = {5, 2, 9, 3, 10, 43, 12, 4, 8};
+    int n = arr.size();
+    arr = stableDualPivotQuickSort(arr, 0, n-1);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
-    }
-    cout<<endl;
-    vector<pair<int, int>> v = { {6, 1}, {5, 5}, {3, 1}, {5, 2}, {7, 1}, {10, 9}, {5, 7} };
-    vector_sort(v, 0, v.size() - 1);
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout <<"{" <<v[i].first << "," << v[i].second<<"} ";
     }
     return 0;
 }
