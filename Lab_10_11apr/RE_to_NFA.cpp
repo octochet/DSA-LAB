ac38119f2e324f9561b75284c7dfab5774f124af
octochet <cs21b017@iittp.ac.in>
2023-04-17 09:54:15 +0530

    lab 10

--- Lab_10_11apr/RE_to_NFA.cpp
+++ Lab_10_11apr/RE_to_NFA.cpp
@@ -1,10 +1,34 @@
 #include <iostream>
+#include <regex>
 
 using namespace std;
 
-//print hello world
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
-}
+/*
+input:
+a regular expression, R, in the form of a string
+
+output:
+the corresponding NFA
+
+solution:
+1. create a new NFA, N
+2. for each character in R
+    if the character is a literal
+        create a new state, s
+        add a transition from the current state to s on the character
+        make s the current state
+    else if the character is a concatenation operator
+        do nothing
+    else if the character is a union operator
+        create a new state, s
+        add a transition from the current state to s on the empty string
+        make s the current state
+    else if the character is a kleene star operator
+        create a new state, s
+        add a transition from the current state to s on the empty string
+        add a transition from s to the current state on the empty string
+        make s the current state
+3. add a final state to N
+4. add a transition from the current state to the final state on the empty string
+5. return N
+*/
